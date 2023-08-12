3a91de7748332d3e1f34eabd1e0b66b89b5ad54e
yansebutong <yansebtuong@gmail.com>
2023-08-12 10:19:27 +0800

    add web hello world

--- src/main/java/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-
-    public void showMessage() {
-        System.out.println("Hello World!");
-    }
-
-}
---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/demo01")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello Tomcat!");
+    }
+}
